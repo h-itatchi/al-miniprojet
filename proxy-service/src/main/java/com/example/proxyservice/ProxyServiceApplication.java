72bca01c47c2bbe3ce71c411f12663367d9c8bb6
ITATCHI <hamza.benbelkacem@univ-constantine2.dz>
2021-03-11 21:50:14 +0100

    course service 100% teachers service 00% config service 50% proxy service 60% views service 00%

--- proxy-service/src/main/java/com/example/proxyservice/ProxyServiceApplication.java
+++ proxy-service/src/main/java/com/example/proxyservice/ProxyServiceApplication.java
@@ -2,12 +2,20 @@ package com.example.proxyservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
+@EnableZuulProxy
 public class ProxyServiceApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(ProxyServiceApplication.class, args);
+    }
+
+    @Bean
+    public SimpleFilter simpleFilter() {
+        return new SimpleFilter();
     }
 
 }

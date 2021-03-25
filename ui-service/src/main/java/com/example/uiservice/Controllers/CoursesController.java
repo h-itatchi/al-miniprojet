7644eb3abde895176e62d5445bb3ab5f8fb676e5
Hamza Benbelkacem <hamza.benbelkacem@univ-constantine2.dz>
2021-03-25 23:35:41 +0100

    course-service 100% teachers-service 100% config-service 50% proxy-service 60% discovery-service 00% UI-service 40%

--- ui-service/src/main/java/com/example/uiservice/Controllers/CoursesController.java
+++ ui-service/src/main/java/com/example/uiservice/Controllers/CoursesController.java
@@ -2,24 +2,35 @@ package com.example.uiservice.Controllers;
 
 import com.example.uiservice.DATA.Entities.Teacher;
 import com.example.uiservice.DATA.Entities.Work;
+import com.example.uiservice.DATA.Repositories.Implimentations.CourseRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.validation.Errors;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.context.request.WebRequest;
 import org.springframework.web.servlet.ModelAndView;
 
-import javax.servlet.http.HttpServletRequest;
 import java.time.LocalDate;
+import java.util.ArrayList;
 
 @Controller
 public class CoursesController {
 
-    @GetMapping("/course/add-work")
-    public String showWorkForm(WebRequest request, Model model) {
-        Work w = new Work();
+    @Autowired
+    public CourseRepository repository;
+    private final ArrayList<Work> works;
+
+    public CoursesController() {
+        works = new ArrayList<>(getWorks());
+    }
+
+    @GetMapping(value = {"/course/works/add-work/"})
+    public String showWorkForm(Model model) {
+        Work w;
+        w = new Work();
         w.setTitle("VUE JS");
         w.setDescription("AL");
         w.setDate(LocalDate.now());
@@ -27,8 +38,8 @@ public class CoursesController {
         return "/addwork";
     }
 
-    @PostMapping("/course/add-work")
-    public ModelAndView test(@ModelAttribute("work") final Work work, final HttpServletRequest request, final Errors errors) {
+    @PostMapping("/course/works/add-work/")
+    public ModelAndView addUpdateWork(@ModelAttribute("work") final Work work) {
         System.out.println(work.getTitle() + " " + work.getType());
         Teacher teacher = new Teacher();
         ModelAndView mav = new ModelAndView("/homepage", "teacher", teacher);
@@ -37,15 +48,42 @@ public class CoursesController {
 
     @GetMapping("/course/works")
     public String showWorks(WebRequest request, Model model) {
-        Work w = new Work();
-        w.setTitle("VUE JS");
-        w.setDescription("AL");
-        w.setType("TP");
-        w.setLinkToPDF("http://wow");
-        w.setDate(LocalDate.now());
-        model.addAttribute("w", w);
+        model.addAttribute("works", works);
         model.addAttribute("name", "AL");
+        model.addAttribute("usertype", "teacher");
         return "/CoursePage";
+    }
+
+    @GetMapping("/course/works/delete/{id}")
+    public String deleteWork(@PathVariable String id) {
+        works.removeIf(work -> Long.parseLong(id) == work.getId());
+        return "redirect:/course/works";
+    }
+
+    @GetMapping("/course/works/update/{id}")
+    public String updateWork(@PathVariable String id, Model model) {
+        Work work = works.stream().filter(w -> Long.parseLong(id) == w.getId()).findFirst().get();
+        model.addAttribute("work", work);
+        return "/addwork";
+    }
+
+    private ArrayList<Work> getWorks() {
+        ArrayList<Work> temp = new ArrayList<>();
+        Work w;
+        for (int i = 0; i < 10; i++) {
+            w = new Work();
+            w.setId(i);
+            w.setTitle("Patron abstract factory");
+            w.setDescription("suivre l'exemple du patron abstract factory du document pdf " +
+                    "en le créant et l'implémentant en java avec  visual paradigme et l'exécuter par la suite sur eclipse ou autre IDE java.\n" +
+                    "\n" +
+                    "Il est recommandé de faire aussi les patrons  singleton et prototype");
+            w.setType("TP");
+            w.setLinkToPDF("https://elearning.univ-constantine2.dz/elearning/course/view.php?id=1149");
+            w.setDate(LocalDate.now());
+            temp.add(w);
+        }
+        return temp;
     }
 
 }
